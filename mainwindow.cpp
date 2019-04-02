bcdbc4e10f89e6a70a2e627735d0e5b5273f331c
Roman Luitko <dossalab@pm.me>
2019-04-02 08:06:22 +0300

    Fix copy-paste errors in constructor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,10 +11,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     com1 = new SerialTester(ui->COM1_portBox, ui->COM1_status, ui->COM1_activeBox, ui->COM1_transmittedData, ui->COM1_receivedData);
-    com2 = new SerialTester(ui->COM2_portBox, ui->COM2_status, ui->COM2_activeBox, ui->COM1_transmittedData, ui->COM2_receivedData);
-    com3 = new SerialTester(ui->COM3_portBox, ui->COM3_status, ui->COM3_activeBox, ui->COM1_transmittedData, ui->COM3_receivedData);
-    com4 = new SerialTester(ui->COM4_portBox, ui->COM4_status, ui->COM4_activeBox, ui->COM1_transmittedData, ui->COM4_receivedData);
-    com5 = new SerialTester(ui->COM5_portBox, ui->COM5_status, ui->COM5_activeBox, ui->COM1_transmittedData, ui->COM5_receivedData);
+    com2 = new SerialTester(ui->COM2_portBox, ui->COM2_status, ui->COM2_activeBox, ui->COM2_transmittedData, ui->COM2_receivedData);
+    com3 = new SerialTester(ui->COM3_portBox, ui->COM3_status, ui->COM3_activeBox, ui->COM3_transmittedData, ui->COM3_receivedData);
+    com4 = new SerialTester(ui->COM4_portBox, ui->COM4_status, ui->COM4_activeBox, ui->COM4_transmittedData, ui->COM4_receivedData);
+    com5 = new SerialTester(ui->COM5_portBox, ui->COM5_status, ui->COM5_activeBox, ui->COM5_transmittedData, ui->COM5_receivedData);
 }
 
 MainWindow::~MainWindow()
